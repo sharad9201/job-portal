655b6d92ea692420a6f0bddd5691dd8a4670a320
Apple <apple@Apples-MacBook-Pro.local>
2022-11-08 00:14:38 +0545

    admins  frontent

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-{{-- <div class="modal-dialog modal-dialog-scrollable">
+    {{-- <div class="modal-dialog modal-dialog-scrollable">
     <table border="1px">
         <tr>
             <th>Title</th>
@@ -27,25 +27,26 @@
 
 
 
-<div class=" container ml-2 px-2">
-    @foreach ($posts as $post)
-    <div class="card " style="width: 18rem;">
-    
-        <img class="card-img-top"  src="{{ asset('/uploads/images/' . $post->image) }}" alt="Card image cap">
-        <div class="card-body ">
-          <h5 class="card-title">  {{$post['title']}}</h5>
+    <div class=" d-flex justify-content-around" ml-2 px-2>
+        <div class="row d-flex justify-content-around">
+        @foreach ($posts as $post)
+            {{-- <div class="flex-row"> --}}
+               
 
-          <p class="card-text"> {{ \Illuminate\Support\Str::words($post->description, 10)}} </p>
-          <a href="{{route('posts.view_more',$post->id)}}" class="btn btn-primary">View more..</a>
-        </div>
+                    <div class="card mt-4" style="width: 20rem;">
+
+                        <img class="card-img-top" src="{{ asset('/uploads/images/' . $post->image) }}" alt="Card image cap" style="height: 14rem;">
+                        <div class="card-body ">
+                            <h5 class="card-title"> {{ $post['title'] }}</h5>
+        
+                            <p class="card-text"> {{ \Illuminate\Support\Str::words($post->description, 10) }} </p>
+                            <a href="{{ route('posts.view_more', $post->id) }}" class="btn btn-primary">View more..</a>
+                        </div>
+                    </div>
+               
+            {{-- </div> --}}
+           
+        @endforeach
     </div>
-    
-    @endforeach
-</div>
-
-
-
-
+    </div>
 @endsection
-
-
