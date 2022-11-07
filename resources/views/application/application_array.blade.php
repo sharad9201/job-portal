dbc526755608151da41a9b746d49cd68052f09cc
Apple <apple@Apples-MacBook-Pro.local>
2022-11-07 23:31:42 +0545

    application frontent

--- resources/views/application/application_array.blade.php
+++ resources/views/application/application_array.blade.php
@@ -1,27 +1,29 @@
-application
-
-
-<div class="modal-dialog modal-dialog-scrollable">
-    <table border="1px">
-        <tr>
-            <th>Users name</th>
-            <th>Description</th>
-            <th>Image</th>
-        </tr>
-        @foreach ($applications as $application)
-        <tr>
-            <td>{{$application['name']}}</td>
-            <td>{{$application['description']}}</td>
-            <td><img src="{{ asset('/uploads/images/' . $application->image) }}" alt="" width="150px"
-                height="150px"></td>
-            </td>
-            <td>
-                <a href="{{route('download',$application->id)}}">Download</a>
-            </td>
-            
+@extends('layouts.app')
+@section('content')
+<div>
+    <h2> Application Submitted by Applicants</h2>
+        <table class="table" >
+            <tr>
+                <th>Users name</th>
+                <th>Description</th>
+                <th>CV</th>
             </tr>
-            
-        </tr>
-        @endforeach
-    </table>
-</div>  
+            @foreach ($applications as $application)
+            <tr>
+                <td>{{$application['name']}}</td>
+                <td>{{$application['description']}}</td>
+                <td><img src="{{ asset('/uploads/images/' . $application->image) }}" alt="" width="150px"
+                    height="150px"></td>
+                </td>
+                <td>
+                    <a href="{{route('download',$application->id)}}" class="btn btn-primary">Download CV</a>
+                </td>
+                
+                </tr>
+                
+            </tr>
+            @endforeach
+        </table>
+</div>
+@endsection
+
