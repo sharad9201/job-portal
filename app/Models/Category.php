e4baabcfad1775cbeeaaa0e81122231ed83b87e0
sharad9201 <sharad.rae19@gmail.com>
2022-11-15 08:41:43 +0545

    addind category table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name','post_id'];
+
+    public function post()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
 }
--- database/migrations/2022_11_14_064728_create_categories_table.php
+++ database/migrations/2022_11_14_064728_create_categories_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('post_id')->constrained();
             $table->timestamps();
         });
     }
